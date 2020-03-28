a4bfcfbb8fe34fb177a9def6db309f9a2451430c
David Dionis <daviddionissoler@gmail.com>
2020-03-28 18:33:57 +0100

    Funciona todas las funciones de lista y la mayoria de Productos. Falta pulir codigo

--- src/routes/listas.js
+++ src/routes/listas.js
@@ -2,9 +2,11 @@ const router=require('express').Router();
 const pool=require('../database');
 
 router.get('/', async(req,res)=>{
-    const listas=await pool.query('SELECT * FROM listas');
-    console.log(listas);
-    res.render('listas/todas_listas', {listas,total_num: listas.length});
+    pool.query('SELECT * FROM listas')
+        .then(response=>{
+            res.render('listas/todas_listas', {listas: response,total_num: response.length})
+        })
+        .catch(err=>res.send(err));
 });
 
 router.get('/add', async(req,res)=>{
@@ -16,19 +18,19 @@ router.post('/add', async(req,res)=>{
         .catch(err=>res.send(err));
 });
 
-router.get('/:id_lista/delete_all', async(req,res)=>{
-    pool.query('DELETE FROM productos WHERE lista_id = ?', [req.params.id_lista])
+router.get('/:lista_id/delete_all', async(req,res)=>{
+    pool.query('DELETE FROM productos WHERE lista_id = ?', [req.params.lista_id])
         .then(response=>{
-            res.redirect(`/listas/${req.params.id_lista}`);
+            res.redirect(`/listas/${req.params.lista_id}`);
         })
         .catch(err=>res.send(err));
     
 
 });
-router.get('/:id_lista/delete_list', async(req,res)=>{
-    pool.query('DELETE FROM productos WHERE lista_id = ?', [req.params.id_lista])
+router.get('/:lista_id/delete_list', async(req,res)=>{
+    pool.query('DELETE FROM productos WHERE lista_id = ?', [req.params.lista_id])
         .then(response=>{
-            pool.query('DELETE FROM listas WHERE lista_id = ?',[req.params.id_lista], (err)=>{
+            pool.query('DELETE FROM listas WHERE lista_id = ?',[req.params.lista_id], (err)=>{
                 if(err){
                     res.send(err);
                 }else{
@@ -40,18 +42,21 @@ router.get('/:id_lista/delete_list', async(req,res)=>{
         .catch(err=>res.send(err));
     
 });
-router.get('/:id_lista/edit_list', async(req,res)=>{
-    const data=await pool.query(`SELECT * FROM listas WHERE ID=${req.params.id_lista}`);
-    res.render('listas/edit_list', {list:data[0]});
+router.get('/:lista_id/edit_list', async(req,res)=>{
+    pool.query(`SELECT * FROM listas WHERE lista_id=${req.params.lista_id}`)
+        .then(response=>res.render('listas/edit_list', {list:response[0]}))
+        .catch(err=>res.send(err));
 });
-router.post('/:id_lista/edit_list', async(req,res)=>{
-    await pool.query(`UPDATE listas SET fullname = '${req.body.fullname}' WHERE ID=${req.params.id_lista}`);
-    res.redirect('/');
+router.post('/:lista_id/edit_list', async(req,res)=>{
+    const {name_lista}=req.body;
+    pool.query('UPDATE listas SET ? WHERE lista_id = ? ', [{name_lista}, req.params.lista_id])
+        .then(response=>res.redirect('/'))
+        .catch(err=>res.send(err));
 });
-router.get('/:id_lista', async(req,res)=>{
-    const prods=await pool.query(`SELECT * FROM ${req.params.id_lista}_table`);
+router.get('/:lista_id', async(req,res)=>{
+    const prods=await pool.query(`SELECT * FROM productos NATURAL JOIN listas`);
     console.log(prods);
-    res.render('listas/elementos_lista', {prods,total_num: prods.length,id_lista: req.params.id_lista});
+    res.render('listas/elementos_lista', {prods,total_num: prods.length,lista_id: req.params.lista_id});
 });
 
 module.exports=router;
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,42 +1,42 @@
 const router=require('express').Router();
 const pool=require('../database');
 
-router.get('/listas/:id_lista/add_prod', async(req,res)=>{
-    res.render('productos/nuevo_prod', {id_lista:req.params.id_lista});
+router.get('/listas/:lista_id/add_prod', async(req,res)=>{
+    const {lista_id}=req.params;
+    res.render('productos/nuevo_prod', {lista_id});
 });
-router.post('/listas/:id_lista/add_prod', async(req,res)=>{
-    //console.log(req.body.txt_titulo);
-    const {txt_fullname} = req.body; //Crear una variable String con el mismo nombre que el elemento del array
+router.post('/listas/:lista_id/add_prod', async(req,res)=>{
+    const {name_producto} = req.body; //Crear una variable String con el mismo nombre que el elemento del array
     const errores=[];   
 
-    if(!txt_fullname){
+    if(!name_producto){
         errores.push({texto_error: 'Porfavor, inserte un título.'});
     }
     if(errores.length>0){
-        res.redirect(`/listas/${req.params.id_lista}`);
+        res.redirect(`/listas/${req.params.lista_id}`);
     }else{
-        await pool.query(`INSERT INTO ${req.params.id_lista}_table set ?`,[{fullname:txt_fullname}])
+        pool.query(`INSERT INTO productos SET ?`,[{name_producto, lista_id: req.params.lista_id}])
         req.flash('msg_exito', 'Producto añadido con éxito');
-        res.redirect(`/listas/${req.params.id_lista}`);
+        res.redirect(`/listas/${req.params.lista_id}`);
     }
 });
 
 
-router.get('/listas/:id_lista/edit_prod/:id_prod',async(req,res)=>{
-    const prod=await pool.query(`SELECT * FROM ${req.params.id_lista}_table WHERE ID=${req.params.id_prod}`);
+router.get('/listas/:lista_id/edit_prod/:id_prod',async(req,res)=>{
+    const prod=await pool.query(`SELECT * FROM ${req.params.lista_id}_table WHERE ID=${req.params.id_prod}`);
     console.log(prod);
-    res.render('productos/editar_prod', {prod:prod[0],id_lista:req.params.id_lista});
+    res.render('productos/editar_prod', {prod:prod[0],lista_id:req.params.lista_id});
 });
-router.put('/listas/:id_lista/edit_prod/:id_prod',async(req,res)=>{
+router.put('/listas/:lista_id/edit_prod/:id_prod',async(req,res)=>{
     const {txt_fullname}=req.body;
-    await pool.query(`UPDATE ${req.params.id_lista}_table set ? WHERE ID=?`, [req.params.id_prod, {ID:req.params.id_prod, fullname: txt_titulo}]);
+    await pool.query(`UPDATE ${req.params.lista_id}_table set ? WHERE ID=?`, [req.params.id_prod, {ID:req.params.id_prod, fullname: txt_titulo}]);
     req.flash('msg_exito', 'Producto actualizado correctamente');
-    res.redirect(`/listas/${req.params.id_lista}`);
+    res.redirect(`/listas/${req.params.lista_id}`);
 });
-router.get('/listas/:id_lista/delete_prod/:id_prod',async(req,res)=>{
-    await pool.query(`DELETE FROM ${req.params.id_lista}_table WHERE ID=?`,[req.params.id_prod]);
+router.get('/listas/:lista_id/delete_prod/:id_prod',async(req,res)=>{
+    await pool.query(`DELETE FROM ${req.params.lista_id}_table WHERE ID=?`,[req.params.id_prod]);
     req.flash('msg_exito', 'Prodcuto eliminado correctamente');
-    res.redirect(`/listas/${req.params.id_lista}`);
+    res.redirect(`/listas/${req.params.lista_id}`);
 });
 
 
